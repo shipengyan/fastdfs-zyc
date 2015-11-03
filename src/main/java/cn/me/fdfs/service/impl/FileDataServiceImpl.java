0e556d8eb254a266b072087928dbd2918d0822df
shi.pengyan <shi.pengyan@zte.com.cn>
2015-11-03 10:04:24 +0800

    fix created is null，使用log4j.properties配置

--- src/main/java/cn/me/fdfs/service/impl/FileDataServiceImpl.java
+++ src/main/java/cn/me/fdfs/service/impl/FileDataServiceImpl.java
@@ -14,49 +14,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Administrator
- * Date: 12-9-3
- * Time: 下午7:09
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: Administrator Date: 12-9-3 Time: 下午7:09 To
+ * change this template use File | Settings | File Templates.
  */
 @Service
 public class FileDataServiceImpl extends BaseService implements FileDataService {
 
-    @Override
-    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
-    public List<Fdfs_file> getFileListByGroupName(String groupName) {
-        List<Fdfs_file> files = new ArrayList<Fdfs_file>();
-        Session session = getSession();
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+	public List<Fdfs_file> getFileListByGroupName(String groupName) {
+		List<Fdfs_file> files = new ArrayList<Fdfs_file>();
+		Session session = getSession();
 
-        StringBuilder queryString = new StringBuilder(
-                "from Fdfs_file as f where  f.file_id like '" + groupName + "%'");
-        Query query = session.createQuery(queryString.toString());
-        files = query.list();
+		StringBuilder queryString = new StringBuilder("from Fdfs_file as f where  f.file_id like '" + groupName + "%'");
+		Query query = session.createQuery(queryString.toString());
+		files = query.list();
 
-        return files;
-    }
+		return files;
+	}
 
-    @Override
-    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
-    public DownloadFileRecord getDownloadFileRecordByIpAndFileId(String ip, String fileId) {
-        DownloadFileRecord downloadFileRecord=new DownloadFileRecord();
-        Session session = getSession();
-        StringBuilder queryString = new StringBuilder("  from DownloadFileRecord df where df.src_ip='"+ip+"' and df.fileId='"+fileId+"'");
-        Query query = session.createQuery(queryString.toString());
-          List<DownloadFileRecord> list=query.list();
-        if(list.isEmpty()){
-            return null;
-        }else {
-            return list.get(0);
-        }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+	public DownloadFileRecord getDownloadFileRecordByIpAndFileId(String ip, String fileId) {
+		DownloadFileRecord downloadFileRecord = new DownloadFileRecord();
+		Session session = getSession();
+		StringBuilder queryString = new StringBuilder("  from DownloadFileRecord df where df.src_ip='" + ip
+				+ "' and df.fileId='" + fileId + "'");
+		Query query = session.createQuery(queryString.toString());
+		List<DownloadFileRecord> list = query.list();
+		if (list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(0);
+		}
 
-    }
+	}
 
-    @Override
-    @Transactional(propagation = Propagation.REQUIRED)
-    public void saveDF(DownloadFileRecord downloadFileRecord) {
-        Session session = getSession();
-        session.saveOrUpdate(downloadFileRecord);
-    }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void saveDF(DownloadFileRecord downloadFileRecord) {
+		Session session = getSession();
+		session.saveOrUpdate(downloadFileRecord);
+	}
 }
